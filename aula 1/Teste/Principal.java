09d7f016d504584afa73b8fef68b7f0a219b1867
Renanlucass <renanlucas0014@gmail.com>
2025-04-23 08:45:15 -0300

    a

---
+++ aula 1/Teste/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String args[]){
+        contaBancaria = new ContaBancaria();
+        contaBancaria.numero = "12*0";
+        contaBancaria.titulo = "Robert";
+        contaBancaria.saldo = 0;
+    }
+}
---
+++ aula 1/hello world/ContaBancaria.java
@@ -0,0 +1,12 @@
+public class ContaBancaria {
+    private int numero;
+    private String titulo;
+    private double saldo;
+
+    public double sacar(){
+        return this.saldo;
+    }
+    public double depositar(){
+        return this.saldo;
+    }
+}
