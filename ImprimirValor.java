6f38c899b03c832e0f3bb6fd8cf4ba06d354d3c3
Renanlucass <renanlucas0014@gmail.com>
2025-04-30 08:49:38 -0300

    a

---
+++ ImprimirValor.java
@@ -0,0 +1,10 @@
+
+public class ImprimirValor {
+    public static void main(String[] args) {
+    int i = 150;
+    while(i < 301){
+        System.out.print(i+",");
+        i = i+1;
+    }
+}
+}
---
+++ MultTres.java
@@ -0,0 +1,10 @@
+public class MultTres {
+    public static void main(String[] args) {
+        int mult = 1;
+        while(mult < 101){
+            int valor = 3 * mult;
+            mult = mult + 1;
+            System.out.print(valor+",");
+        }
+    }
+}
