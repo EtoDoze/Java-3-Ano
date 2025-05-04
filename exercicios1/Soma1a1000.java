5b4432057d43ad322e5506e9d0ff935d1eb00bc6
Eto12 <reverso12yt@gmail.com>
2025-05-04 01:05:53 -0300

    a

--- exercicios1/Soma1a1000.java
+++ exercicios1/Soma1a1000.java
@@ -2,9 +2,11 @@ package exercicios1;
 
 public class Soma1a1000 {
     public static void main(String[] args) {
-        for(int i=1; i<=1000;i++){
-            
-            System.out.println();
+        int soma = 0;
+        for(int i=0; i<=1000;i++){
+             soma += i;
         }
+
+        System.out.println("A soma é:"+soma);
     }
 }
