d61d929070c1cb0b3d1a98e8bd744a60db62ecc2
Lorranyy <capic.2022118isinf0799@aluno.ifpi.edu.br>
2025-05-22 10:20:41 -0300

    a

---
+++ Heranca/heranca.java
@@ -0,0 +1,15 @@
+//Herança e sobrecarga/sobrescrita de métodos
+//existem superclasses, 
+//metodo construtor tem o nome da classe, é possivel iniciar um atributo
+//sobrecarga/sobrescrita é reescrever um metodo de outra classe, eles devem ter a mesma assinatura, retornar a mesma coisa e ter os mesmos parametros
+//só pode herdar de 1 superclasse
+// final é uma classe constante, const
+// 
+
+package heranca;
+
+public class heranca {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Heranca/main.java
@@ -0,0 +1,14 @@
+//Herança e sobrecarga/sobrescrita de métodos
+//existem superclasses, 
+//metodo construtor tem o nome da classe, é possivel iniciar um atributo
+//sobrecarga/sobrescrita é reescrever um metodo de outra classe, eles devem ter a mesma assinatura, retornar a mesma coisa e ter os mesmos parametros
+//só pode herdar de 1 superclasse
+// final é uma classe constante, const
+// 
+
+public class main {
+    public static void main(String[] args) {
+        Cachorro meuCachorro = new Cachorro("Bolt", "Labrador");
+        Gato meuGato = new Gato("Whiskers");
+    }
+}
